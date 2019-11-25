def2131ee18c8f1f58ba38a4c38dc8f1a647af14
Maher Santina <maher.santina90@gmail.com>
2019-11-25 20:25:01 +1100

    Removed unused code

--- Sources/DSAuth/configure.swift
+++ Sources/DSAuth/configure.swift
@@ -9,75 +9,7 @@ import Foundation
 import Vapor
 import FluentMySQL
 
-public protocol DSAuthDatabaseConfigurable {
-    var dsAuthDatabaseHostName: String { get }
-    var dsAuthDatabasePort: Int { get }
-    var dsAuthDatabaseUsername: String { get }
-    var dsAuthDatabasePassword: String { get }
-    var dsAuthDatabaseDatabase: String { get }
-    var dsAuthDatabaseTestDatabase: String { get }
-}
-
 public class DSAuthMain {
-
-    let dataSource: DSAuthDatabaseConfigurable
-
-    public init(dataSource: DSAuthDatabaseConfigurable) {
-        self.dataSource = dataSource
-    }
-
-//    public func authConfigure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-//        // Register providers first
-//        try services.register(FluentProvider())
-//        try services.register(MySQLProvider())
-//
-//        var commandConfig = CommandConfig.default()
-//        commandConfig.useFluentCommands()
-//        services.register(commandConfig)
-//
-//        var databases = DatabasesConfig()
-//
-//        var databaseName: String = ""
-//        switch env {
-//        case .testing:
-//            databaseName = dataSource.dsAuthDatabaseTestDatabase
-//        default:
-//            databaseName = dataSource.dsAuthDatabaseDatabase
-//        }
-//
-//        let mysql = MySQLDatabase(config: MySQLDatabaseConfig(
-//            hostname: dataSource.dsAuthDatabaseHostName,
-//            port: dataSource.dsAuthDatabasePort,
-//            username: dataSource.dsAuthDatabaseUsername,
-//            password: dataSource.dsAuthDatabasePassword,
-//            database: databaseName
-//            )
-//        )
-//        databases.add(database: mysql, as: .mysql)
-//        services.register(databases)
-//
-//
-//        // Configure migrations
-//        var migrations = MigrationConfig()
-//        migrations.add(migration: EnableReferencesMigration.self, database: .mysql)
-//
-//        migrations.add(model: UserRow.self, database: .mysql)
-//
-//        migrations.add(model: OrganizationRow.self, database: .mysql)
-//
-//        migrations.add(model: RoleRow.self, database: .mysql)
-//        migrations.add(migration: RolesMigration.self, database: .mysql)
-//
-//        migrations.add(model: LoginRow.self, database: .mysql)
-//
-//        migrations.add(migration: LoginOnOrganizationMigration.self, database: .mysql)
-//
-//        migrations.add(migration: Login_RoleRow.self, database: .mysql)
-//        migrations.add(migration: User_LoginRow.self, database: .mysql)
-//
-//        migrations.add(migration: UniqueLoginMigration.self, database: .mysql)
-//        services.register(migrations)
-//    }
 
     public func authConfigure(migrations: inout MigrationConfig) {
         migrations.add(migration: EnableReferencesMigration.self, database: .mysql)
