d39f06cdea5758dd53232ebec31b0d0554427ab6
Maher Santina <maher.santina90@gmail.com>
2020-01-10 19:14:52 +1100

    Added new DSCore

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.1"),
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
-        .package(url: "https://github.com/MaherKSantina/DSCore.git", from: "0.2.0"),
+        .package(url: "https://github.com/MaherKSantina/DSCore.git", from: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
